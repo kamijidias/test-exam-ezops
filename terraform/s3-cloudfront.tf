ff22af4323d5299d17bc87f3bd892dda932ce4e2
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2024-07-13 23:15:30 -0300

    Deployments

--- terraform/s3-cloudfront.tf
+++ terraform/s3-cloudfront.tf
@@ -16,22 +16,6 @@ resource "aws_s3_bucket" "frontend_bucket" {
   }
 }
 
-resource "aws_s3_bucket_policy" "frontend_bucket_policy" {
-  bucket = aws_s3_bucket.frontend_bucket.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = "s3:GetObject",
-        Resource  = "${aws_s3_bucket.frontend_bucket.arn}/*"
-      }
-    ]
-  })
-}
-
 resource "aws_cloudfront_distribution" "frontend_distribution" {
   origin {
     domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
